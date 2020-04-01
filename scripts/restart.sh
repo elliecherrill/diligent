65687f63e8581c819c8eca9e47fc313ed5b83104
Helena  Cherrill <helena.cherrill17@imperial.ac.uk>
2020-04-01 17:24:40 +0100

    Update restart.sh

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -22,13 +22,7 @@ gunicorn backend.wsgi:app \
            --enable-stdio-inheritance \
            --daemon
 
-if ! pgrep -x "node" > /dev/null
-then
-  echo Node not running already
-else
-  echo Killing and restarting node
-  killall node
-fi
+killall node
 
 cd frontend/
 npx serve -s build -l 3000 &>/dev/null &

448a13a2b77e3b1fe70116b8dd49634ba4472cf2
Helena Cherrill <hac17@ic.ac.uk>
2020-04-07 10:32:38 +0100

    Add Ignore Advice button

--- inspections/src/main/java/util/Feedback.java
+++ inspections/src/main/java/util/Feedback.java
@@ -5,11 +5,13 @@ public class Feedback {
     private final int lineNumber;
     private final String errorMsg;
     private final String filename;
+    private boolean fixed;
 
     public Feedback(int lineNumber, String errorMsg, String filename) {
         this.lineNumber = lineNumber;
         this.errorMsg = errorMsg;
         this.filename = filename;
+        fixed = false;
     }
 
     @Override
@@ -25,11 +27,27 @@ public class Feedback {
         return sb.toString();
     }
 
+    // TODO: How to feedback when Ignore Advice button has been pressed and *use* this?
     public String toHTMLString() {
-        return "<div style=\"border: red solid 2px; width: 40%; margin-top: 2.5%; margin-left: 2.5%;border-radius: 5px; padding: 1%;font-family: Roboto, Helvetica, Arial, sans-serif;\">\n" +
-                "   <p style=\"font-weight: 500;\"> " + errorMsg + " </p>\n" +
-                "   <p> " + filename + " > " + lineNumber + " </p>\n" +
-                "</div>\n";
+        return "<div style=\"display: flex;flex-direction: row; align-items: center;margin-top: 2.5%; margin-left: 2.5%;\">\n" +
+                "   <div style=\"border: " + getColour() + " solid 2px; width: 40%; border-radius: 5px; padding: 1%;font-family: Roboto, Helvetica, Arial, sans-serif;\">\n" +
+                "       <p style=\"font-weight: 500;\"> " + errorMsg + " </p>\n" +
+                "       <p> " + filename + " > " + lineNumber + " </p>\n" +
+                "   </div>\n" +
+                "   <div style=\"margin-left: 2.5%;\">\n" +
+                "       <button style=\"background-color: #34558b; color: white; border: none; border-radius: 5px;font-family: Roboto, Helvetica, Arial, sans-serif;\">\n" +
+                "           Ignore Advice\n" +
+                "       </button>\n" +
+                "   </div>\n" +
+                "</div>";
+    }
 
+    // TODO: When to update fixed?
+    public void setToFixed() {
+        fixed = true;
+    }
+
+    private String getColour() {
+        return fixed ? "green" : "red";
     }
 }

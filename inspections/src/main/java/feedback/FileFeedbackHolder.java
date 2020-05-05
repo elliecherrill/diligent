b81260f1020f6d1a07b67b3af83c48be648f40ed
Helena Cherrill <hac17@ic.ac.uk>
2020-05-05 11:44:44 +0100

    Add praise messages for different priority levels

--- inspections/src/main/java/feedback/FileFeedbackHolder.java
+++ inspections/src/main/java/feedback/FileFeedbackHolder.java
@@ -15,8 +15,12 @@ public class FileFeedbackHolder {
     private final String filename;
     private final String filepath;
 
+    // 0 = high, 1 = medium, 2 = low (TRUE if error has *ever* occurred at that level)
+    private boolean[] priorityErrors;
+
     public FileFeedbackHolder(String filename) {
         feedback = new ConcurrentHashMap<>();
+        priorityErrors = new boolean[3];
         this.filename = filename;
         this.filepath = filename.replace(".java", ".html");
     }
@@ -54,6 +58,7 @@ public class FileFeedbackHolder {
         }
 
         feedback.put(id, newFeedback);
+        priorityErrors[newFeedback.getPriority().getIndex()] = true;
 
         return new Pair<>(isCurrent, feedbackIsNew);
     }
@@ -102,12 +107,67 @@ public class FileFeedbackHolder {
     public String getFeedbackAsHTMLString(List<InspectionPriority> priorities) {
         StringBuffer sb = new StringBuffer();
 
+        sb.append(addPriorityPraise(InspectionPriority.HIGH, priorityErrors[InspectionPriority.HIGH.getIndex()], priorities));
+        sb.append(addPriorityPraise(InspectionPriority.MEDIUM, priorityErrors[InspectionPriority.MEDIUM.getIndex()], priorities));
+        sb.append(addPriorityPraise(InspectionPriority.LOW, priorityErrors[InspectionPriority.LOW.getIndex()], priorities));
+
         for (Map.Entry entry : feedback.entrySet()) {
             Feedback f = (Feedback) entry.getValue();
             sb.append(f.toHTMLString(priorities));
         }
 
         return sb.toString();
+    }
+
+    private String addPriorityPraise(InspectionPriority priority, boolean error, List<InspectionPriority> priorities) {
+        if (!priorities.contains(priority)) {
+            return "";
+        }
+
+        if (!error) {
+            return "<div class=\"feedbackcontainer\" id=\"praise-" + priority + "\">\n" +
+                    "   <div style=\"border: green solid 2px;\" id=\"feedback\">\n" +
+                    "       <p style=\"font-weight: 500;\"> Your code is looking " + getPraiseMessage(priority) + "! You have no " + priority + "-priority errors. </p>\n" +
+                    "   </div>\n" +
+                    "</div>";
+        }
+
+        int fixedCount = getPriorityFixedCount(priority);
+        if (fixedCount > 0) {
+            return "<div class=\"feedbackcontainer\" id=\"praise-" + priority + "\">\n" +
+                    "   <div style=\"border: green solid 2px;\" id=\"feedback\">\n" +
+                    "       <p style=\"font-weight: 500;\"> Your code is looking " + getPraiseMessage(priority) + "! You fixed " + fixedCount + " " + priority + "-priority error" + (fixedCount > 1 ? "s" : "") + ". </p>\n" +
+                    "   </div>\n" +
+                    "</div>";
+        }
+
+        return "";
+    }
+
+    private String getPraiseMessage(InspectionPriority priority) {
+        if (priority == InspectionPriority.HIGH) {
+            return "good";
+        }
+        if (priority == InspectionPriority.MEDIUM) {
+            return "very good";
+        }
+        if (priority == InspectionPriority.LOW) {
+            return "great";
+        }
+        return "";
+    }
+
+    private int getPriorityFixedCount(InspectionPriority priority) {
+        int count = 0;
+
+        for (Map.Entry entry : feedback.entrySet()) {
+            Feedback f = (Feedback) entry.getValue();
+            if (f.isFixed() && f.getPriority() == priority) {
+                count++;
+            }
+        }
+
+        return count;
     }
 
     public String toHTMLString() {
--- inspections/src/main/java/util/InspectionPriority.java
+++ inspections/src/main/java/util/InspectionPriority.java
@@ -1,8 +1,18 @@
 package util;
 
 public enum InspectionPriority {
-    HIGH,
-    MEDIUM,
-    LOW,
-    NONE
+    HIGH(0),
+    MEDIUM(1),
+    LOW(2),
+    NONE(-1);
+
+    private final int index;
+
+    InspectionPriority(int index) {
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
 }

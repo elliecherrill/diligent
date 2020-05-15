e6a3e5989d59f30a23f4e4310809e95177df8f74
Helena Cherrill <hac17@ic.ac.uk>
2020-05-15 10:56:31 +0100

    Add method length inspection

--- python-inspections/src/main/java/inspection/MethodLengthInspection.java
+++ python-inspections/src/main/java/inspection/MethodLengthInspection.java
@@ -47,8 +47,42 @@ public final class MethodLengthInspection extends AbstractBaseJavaLocalInspectio
             @Override
             public void visitPyFunction(PyFunction node) {
                 super.visitPyFunction(node);
-                holder.registerProblem(node, "FUNCTION", ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
+
+                PyStatementList statList = node.getStatementList();
+                PyStatement[] stats = statList.getStatements();
+
+                if (stats.length >= MAX_METHOD_LENGTH) {
+                    holder.registerProblem(node, "Method length should not be longer than " + MAX_METHOD_LENGTH + " statements.", ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
+                }
             }
+//
+//            @Override
+//            public void visitMethod(PsiMethod method) {
+//                super.visitMethod(method);
+//
+//                if (Utils.hasErrorsInFile(method)) {
+//                    return;
+//                }
+//
+//                PsiCodeBlock body = method.getBody();
+//
+//                String filename = method.getContainingFile().getName();
+//                int line = Utils.getLineNumber(method);
+//                FeedbackIdentifier feedbackId = new FeedbackIdentifier(Utils.getPointer(method), "method-length", PsiStmtType.METHOD, line);
+//
+//                if (body != null && body.getStatementCount() >= MAX_METHOD_LENGTH) {
+//                    Feedback feedback = new Feedback(line,
+//                            filename,
+//                            line + "-method-length",
+//                            priority,
+//                            Utils.getClassName(method),
+//                            FeedbackType.METHOD_LENGTH);
+//                    feedbackHolder.addFeedback(holder.getProject(), filename, feedbackId, feedback);
+//                } else {
+//                    feedbackHolder.fixFeedback(holder.getProject(), filename, feedbackId);
+//                }
+//            }
+
         };
     }
 }
